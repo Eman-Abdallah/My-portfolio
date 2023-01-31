69861e82f455295536fa6511f3a47c29996f4525
Eman Abdallah <91523338+Eman-Abdallah@users.noreply.github.com>
2023-01-31 11:07:50 +0200

    add my email

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  name!: string;
+  email!: string;
+  project!: string;
+  interest!: string;
+  details!: string;
   constructor() { }
 
   ngOnInit(): void {
   }
+  textMe(): void {
+    console.log(this.name);
 
+    let email = ['emanshaltoot623@gmail.com'];
+    let link =
+      'mailto:' +
+      email +
+      '?subject=' +
+      this.project +
+      '&body=' +
+      '   Name Is:  ' +
+      this.name +
+      '   From:  ' +
+      this.email +
+      ' Interest:' +
+      this.interest +
+      ' Details: ' +
+      this.details;
+
+    window.location.href = link;
+  }
 }
